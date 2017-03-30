6abcb4320f02b8c61af9b1f14114ae299bf168bf
luongquocan196 <lqa.ntk@gmail.com>
2017-03-31 00:15:39 +0700

    31/3/2017

--- connectivesport/Activities/LoginActivity.cs
+++ connectivesport/Activities/LoginActivity.cs
@@ -12,6 +12,8 @@ using Android.Views;
 using Android.Widget;
 using Microsoft.WindowsAzure.MobileServices;
 using System.Threading.Tasks;
+using Facebook;
+using Newtonsoft.Json.Linq;
 
 namespace connectivesport
 {
@@ -30,6 +32,38 @@ namespace connectivesport
                 bool result = await Authenticate();
                 if (result)
                 {
+                    string token = user.MobileServiceAuthenticationToken;
+
+                    Newtonsoft.Json.Linq.JToken x = await UserManager.DefaultManager.CurrentClient.InvokeApiAsync("/.auth/me");
+                    string s = (string)x[0]["access_token"];
+
+                    FacebookClient fb = new FacebookClient(s);
+                    JToken profile = JToken.Parse(fb.Get("me/?fields=picture,name,email").ToString());
+
+                    var UserTable = UserManager.DefaultManager.CurrentClient.GetTable<User>();
+                    var test = from each in UserTable where each.UserID == (string)profile["id"] select each.UserID;
+                    if (test.Parameters.Count == 0)
+                    {
+                        User nUser = new User() { Username = (string)profile["name"], AvatarURL = (string)profile["picture"]["data"]["url"], Email = (string)profile["email"], LastLocationX = 5.3, LastLocationY = 3.2, UserID = (string)profile["id"], LastLogin = DateTime.Now, RegistrationDate = DateTime.Now };
+                        await UserTable.InsertAsync(nUser);
+
+                        /*JToken friends = JToken.Parse(fb.Get("me/friends").ToString());
+                        for (int i = 0; i < friends["data"].Count(); i++)
+                        {
+                            string id = friends["data"][i]["id"].ToString();
+                            JToken friend = JToken.Parse(fb.Get(id + "/?fields=picture,name").ToString());
+                            string name = friend["name"].ToString();
+                            string url = friend["picture"]["data"]["url"].ToString();
+                            Friend nFriend = new Friend() { AcceptDate = DateTime.Now, valid = true, RequestUserId = (string)friend["id"], AcceptUserId = (string)profile["id"], UserId = "abc"};
+                            await FriendTable.InsertAsync(nFriend);
+                        }*/
+                    }
+
+                    /*var contextEdit = contextRef.Edit();
+                    contextEdit.PutString("FBToken", s);
+                    contextEdit.PutBoolean("IsLogin", true);
+                    contextEdit.Commit();*/
+
                     StartActivity(typeof(Practice));
                 }
             };
